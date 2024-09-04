52c3747e54f828264fb3e584677596d552a27fda
Ian Hanson <ianalexhanson@gmail.com>
2024-09-04 10:14:41 +0100

    first commit

---
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -0,0 +1,35 @@
+import { Example } from "./Example";
+
+import styles from "./Example.module.css";
+
+const DEFAULT_EXAMPLES: string[] = [
+    "What technical specifications have changed from the 2015 Procurement Act?",
+    "What types of contracts are exempt from the 2023 Act?",
+    // "What steps are needed to ensure a supplier is not put at an unfair advantage and/or to avoid competition being distorted?",
+    "At the beginning the procurement process, what are the top ten things I should be considering?",
+    //"What does the RBKC Constitution say about procurement rules and regulations?",
+    "What do I need to know about the changes to the Procurement Act?"
+];
+
+const GPT4V_EXAMPLES: string[] = [
+    "Compare the impact of interest rates and GDP in financial markets.",
+    "What is the expected trend for the S&P 500 index over the next five years? Compare it to the past S&P 500 performance",
+    "Can you identify any correlation between oil prices and stock market trends?"
+];
+
+interface Props {
+    onExampleClicked: (value: string) => void;
+    useGPT4V?: boolean;
+}
+
+export const ExampleList = ({ onExampleClicked, useGPT4V }: Props) => {
+    return (
+        <ul className={styles.examplesNavList}>
+            {(useGPT4V ? GPT4V_EXAMPLES : DEFAULT_EXAMPLES).map((question, i) => (
+                <li key={i}>
+                    <Example text={question} value={question} onClick={onExampleClicked} />
+                </li>
+            ))}
+        </ul>
+    );
+};
---
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -0,0 +1,60 @@
+import { Outlet, NavLink, Link } from "react-router-dom";
+
+import rbkc_logo from "../../assets/RBKC.png";
+
+import styles from "./Layout.module.css";
+
+import { useLogin } from "../../authConfig";
+
+import { LoginButton } from "../../components/LoginButton";
+
+const Layout = () => {
+    return (
+        <div className={styles.layout}>
+            <header className={styles.header} role={"banner"}>
+                <div className={styles.headerContainer}>
+                    <Link to="/" className={styles.headerTitleContainer}>
+                        <h3 className={styles.headerTitle}>GPT4 + RBKC Enterprise data</h3>
+                    </Link>
+                    <nav>
+                        <ul className={styles.headerNavList}>
+                            {/* {<li>
+                                <NavLink to="/" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    Chat
+                                </NavLink>
+                            </li>}
+                            {<li className={styles.headerNavLeftMargin}>
+                               <NavLink to="/qa" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                   Ask a question
+                                </NavLink>
+                            </li>} */}
+
+                            <li className={styles.headerNavLeftMargin}>
+                                <a
+                                    href="https://officesharedservice.sharepoint.com/sites/RBKCProcurementAct2023"
+                                    target={"_blank"}
+                                    title="Procurement SharePoint link"
+                                >
+                                    <img
+                                        src={rbkc_logo}
+                                        alt="RBKC logo"
+                                        // aria-label="Link to github repository"
+                                        width="200px"
+                                        height="40px"
+                                        className={styles.rbkcLogo}
+                                    />
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <h4 className={styles.headerRightText}>Azure OpenAI + AI Search</h4>
+                    {useLogin && <LoginButton />}
+                </div>
+            </header>
+
+            <Outlet />
+        </div>
+    );
+};
+
+export default Layout;
